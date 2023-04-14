764e1f20c926ea27641d47b9231b56a43c6dfc0a
Gabriel-Sousa-Amorim <essecara1310@gmail.com>
2023-04-13 21:33:51 -0300

    Aula 07_Unidades_Medida

---
+++ Workspace/JS/cep.js
@@ -0,0 +1,85 @@
+function cep() {
+    let cep = "034";
+        if (cep[0] == "0") {
+            console.log("São Paulo");
+            if (cep[1] == "1" && cep[2] == "0") {
+                console.log("Centro, Sé, Santa Efigênia, República");
+            }
+            else if (cep[1] == "1" && cep[2] == "1") {
+                console.log("Centro, Santa Cecília/ Pacaembú, Sumaré, Higienópolis, Consolação");
+            }
+            else if (cep[1] == "1" && cep[2] == "2") {
+                console.log("Centro, Sé, Santa Efigênia, República, Centro");
+            }
+            else if (cep[1] == "1" && cep[2] == "3") {
+                console.log("Centro, Consolação, Bela Vista");
+            }
+            else if (cep[1] == "1" && cep[2] == "4") {
+                console.log("Centro, Cerqueira César, Jardim Paulista, Jardim América, Jardim Europa");
+            }
+            else if (cep[1] == "1" && cep[2] == "5") {
+                console.log("Centro, Liberdade, Cambuci, Aclimação, Vila Monumento, Jardim da Glória");
+            }
+            else if (cep[1] == "2" && cep[2] == "0") {
+                console.log("Zona Norte, Santana, Carandiru, Vila Guilherme, Jardim São Paulo")
+            }
+            else if (cep[1] == "2" && cep[2] == "1") {
+                console.log("Zona Norte, Vila Maria, Parque Novo Mundo, Jardim Japão");
+            }
+            else if (cep[1] == "2" && cep[2] == "2") {
+                console.log("Zona Norte, Tucuruvi, Jaçanã, Parque Edu Chaves, Vila Medeiros, Vila Ede")
+            }
+            else if (cep[1] == "2" && cep[2] == "3") {
+                console.log("Zona Norte, Jardim Tremembé, Barro Branco, Água Fria")
+            }
+            else if (cep[1] == "2" && cep[2] == "4") {
+                console.log("Zona Norte, Mandaqui, Imirim, Lausane Paulista, Santa Teresinha")
+            }
+            else if (cep[1] == "2" && cep[2] == "5") {
+                console.log("Zona Norte, Casa Verde, Parque Peruche")
+            }
+            else if (cep[1] == "2" && cep[2] == "6") {
+                console.log("Zona Norte, Vila Nova Cachoeirinha, Jardim Peri Peri")
+            }
+            else if (cep[1] == "2" && cep[2] == "7") {
+                console.log("Zona Norte, Limão, Nossa Senhora do Ó, Itaberaba")
+            }
+            else if (cep[1] == "2" && cep[2] == "8") {
+                console.log("Zona Norte, Brasilândia, Morro Grande")
+            }
+            else if (cep[1] == "2" && cep[2] == "9") {
+                console.log("Zona Norte Freguesia do Ó, Pirituba, Piqueri")
+            }
+            else if (cep[1] == "3" && cep[2] == "0") {
+                console.log("Zona Leste, Belenzinho, Brás, Belém, Pari, Canindé, Catumbi, ParqueS.Jorge") 
+            }
+            else if (cep[1] == "3" && cep[2] == "1") {
+                console.log("Zona Leste, Moóca, Alto da Moóca, Vila Prudente, A. Rosa, Quarta Parada, Parque Moóca, Vila Zelina") 
+            }
+            else if (cep[1] == "3" && cep[2] == "2") {
+                console.log("Zona Leste, Vila Ema, Parque São Lucas, Vila Alpina, Sapopemba") 
+            }
+            else if (cep[1] == "3" && cep[2] == "3") {
+                console.log("Zona Leste, Tatuapé, Vila Formosa, Jardim Colorado, Vila Gomes Cardim, JardimAnália Franco") 
+            }
+            else if (cep[1] == "3" && cep[2] == "4") {
+                console.log("Zona Leste,Vila Carrão, Carrãozinho") 
+            }
+            else if (cep[1] == "3" && cep[2] == "5") {
+                console.log("Zona Leste, Vila Matilde, Cidade Patriarca, Artur Alvim") 
+            }
+            else if (cep[1] == "3" && cep[2] == "6") {
+                console.log("Zona Leste, Belenzinho, Brás, Belém, Pari, Canindé, Catumbi, ParqueS.Jorge") 
+            }
+            else if (cep[1] == "3" && cep[2] == "7") {
+                console.log("Zona Leste, Belenzinho, Brás, Belém, Pari, Canindé, Catumbi, ParqueS.Jorge") 
+            }
+            else if (cep[1] == "3" && cep[2] == "8") {
+                console.log("Zona Leste, Belenzinho, Brás, Belém, Pari, Canindé, Catumbi, ParqueS.Jorge") 
+            }
+            else if (cep[1] == "3" && cep[2] == "9") {
+                console.log("Zona Leste, Belenzinho, Brás, Belém, Pari, Canindé, Catumbi, ParqueS.Jorge") 
+            }
+}
+}
+    cep();

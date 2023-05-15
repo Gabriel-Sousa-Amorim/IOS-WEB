0568d79a63bb5962c3d7e8478083cd8c360e4709
Gabriel-Sousa-Amorim <essecara1310@gmail.com>
2023-05-15 20:38:11 -0300

    06_Arrays

---
+++ Workspace/Javascript/06_Arrays/main.js
@@ -0,0 +1,40 @@
+var unidimensional = [0, 1, 2, 3, 4, 5, 6, 7, 8,9, 10]
+        var bidimensional = [
+    ['Unidade', 'Turma ','Nome', 'Nota', 'Presença'],
+    ['Santana', 'Turma 1','Gabriel', 7, '98%'],
+    ['Santana', 'Turma 1','Rafael', 10, '96%'],
+    ['Santana', 'Turma 1','Pedro', 4, '100%'],
+    ['Santana', 'Turma 1','Carina', 8, '80%'],
+    ['Santana', 'Turma 1','João', 9, '97%']
+];
+        var Array1 = [1,2,3,4,5,6,7,8,9,10];
+
+console.table(bidimensional);
+console.log(bidimensional);
+//(Bidimensional[y][x]);
+console.log(bidimensional[4][1]);
+
+Array1[3] = 3.5;
+console.log(Array1);
+
+//Remove o ultimo elemento da array e que pode ser retornado para uma variável retorna o tamanho dela;
+console.log(Array1.pop()); 
+console.log(Array1);
+
+//Insere o elemento da ultima posição da array e retorna o tamanho dela;
+console.log(Array1.push());
+console.log(Array1);
+
+//Remove o elemento da primeira posição da array e retorna o tamanho dela;
+console.log(Array1.shift());
+console.log(Array1);
+
+//Insere o elemento da primeira posição da array e retorna o tamanho dela;
+console.log(Array1.unshift());
+console.log(Array1);
+
+//Como deletar um elemento de uma array
+delete Array1[1];
+console.log(Array1);
+console.clear
+

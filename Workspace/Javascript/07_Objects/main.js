5df2e7ccd3b9dffaa91f3b77990e2544d64d8a37
Gabriel-Sousa-Amorim <essecara1310@gmail.com>
2023-05-17 21:51:44 -0300

    07_Javascript

---
+++ Workspace/Javascript/07_Objects/main.js
@@ -0,0 +1,17 @@
+//CPF
+let CPF = 53355117889;
+
+CPF = Array.from(String(CPF), Number);
+let res = (CPF[9].String, CPF[10].String);
+console.log(res);
+
+CPF[0] = CPF[0] * 10;
+CPF[1] = CPF[1] * 9;
+CPF[2] = CPF[2] * 8;
+CPF[3] = CPF[3] * 7;
+CPF[4] = CPF[4] * 6;
+CPF[5] = CPF[5] * 5;
+CPF[6] = CPF[6] * 4;
+CPF[7] = CPF[7] * 3;
+CPF[8] = CPF[8] * 2;
+

0868fbcaf2753683101fcc9e4bcfdd86ce85160f
Gabriel-Sousa-Amorim <essecara1310@gmail.com>
2023-05-22 21:44:20 -0300

    09_ForEach

---
+++ Workspace/Javascript/09_ForEach/main.js
@@ -0,0 +1,10 @@
+let array = [1,2,3,4,5];
+console.table(array);
+
+array.forEach(i => console.log("hello world"));
+let new_array = array.map(i => i*2);
+console.table(new_array);
+
+for (let i = 1; i < 57; i++){
+    console.log(`${i})`)
+}

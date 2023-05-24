fcccdadcd6a78486ed196fc514d72aa98c976d19
Gabriel-Sousa-Amorim <essecara1310@gmail.com>
2023-05-23 21:11:14 -0300

    10_Javascript

---
+++ Workspace/Javascript/10_Class_e_Objects/main.js
@@ -0,0 +1,13 @@
+class Info {
+    constructor(nome, sobrenome, idade, endereço) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.idade = idade;
+    this.endereço = endereço;
+    
+}};
+var Gabriel = new Info('Gabriel', 'Sousa', 18, 'Av. Jardim Japão - 1599');
+
+console.table(Info);
+console.table(Gabriel);
+console.table(Gabriel);

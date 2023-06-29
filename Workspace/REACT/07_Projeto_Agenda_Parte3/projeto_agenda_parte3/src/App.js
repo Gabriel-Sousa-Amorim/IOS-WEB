0ae2a7dccca7c18b7cdc3b384ba19c9b65476477
Gabriel-Sousa-Amorim <essecara1310@gmail.com>
2023-06-29 20:03:07 -0300

    React

---
+++ Workspace/REACT/07_Projeto_Agenda_Parte3/projeto_agenda_parte3/src/App.js
@@ -0,0 +1,37 @@
+import Header from './Components/Header.jsx';
+import Tasks from './Components/Tasks.jsx';
+import AddTasks from './Components/AddTasks.jsx';
+
+import './App.css'
+import { useState } from 'react';
+
+function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Consulta médica',
+      day: '5 de Fev as 14:30',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Reunião na Escola',
+      day: '6 de Fev as 13:30',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Compras no Supermercado',
+      day: '7 de Fev as 8:30',
+      reminder: false,
+    },
+  ]);
+  return (
+    <div className="container">
+      <Header title="tarefas" />
+      <AddTasks />
+      <Tasks />
+    </div>
+  );}
+
+  export default App; 

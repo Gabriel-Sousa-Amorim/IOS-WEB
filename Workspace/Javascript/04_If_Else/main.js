6a8a25739735aa74bf09ff9cf1cb2f33c2c49252
Gabriel Sousa <97068125+Gabriel-Sousa-Amorim@users.noreply.github.com>
2024-01-10 10:15:06 -0300

    Create main.js

---
+++ Workspace/Javascript/04_If_Else/main.js
@@ -0,0 +1,33 @@
+    var numero1 = 1;
+    var numero2 = 99999999998;
+
+    console.log(`var numero1 = ${numero1}`);
+    console.log(`var numero2 = ${numero2}`);
+
+if (numero1 % 2 === 0) {
+    if (numero1 > 10) {
+        console.log(`${numero1} é par e maior que 10`);
+    } else {
+        console.log(`${numero1} é par e menor que 10`);
+    }
+} else {
+    if (numero1 > 10) {
+        console.log(`${numero1} é impar e maior que 10`);
+    } else {
+        console.log(`${numero1} é impar e menor que 10`);
+    }
+}
+
+if (numero2 % 2 === 0) {
+    if (numero2 > 10) {
+        console.log(`${numero2} é par e maior que 10`);
+    } else {
+        console.log(`${numero2} é par e menor que 10`);
+    }
+} else {
+    if (numero2 > 10) {
+        console.log(`${numero2} é impar e maior que 10`);
+    } else {
+        console.log(`${numero2} é impar e menor que 10`);
+    }
+};

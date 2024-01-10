cdf67e5af5b396c0cec1506d2669c56d98865847
Gabriel Sousa <97068125+Gabriel-Sousa-Amorim@users.noreply.github.com>
2024-01-10 10:23:03 -0300

    Create main.js

---
+++ Workspace/Javascript/11_DOM_Parte1/main.js
@@ -0,0 +1,7 @@
+        
+        var h1 = document.createElement("h1");
+        document.body.appendChild(h1)
+        h1.innerHTML = "Cadastro Usuário";
+        
+        document.title = "Cadastro empresa Macron";
+

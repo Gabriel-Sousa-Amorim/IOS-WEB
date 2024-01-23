ffc4ea99640be3d9e93065670d70153f19a238f0
Gabriel Sousa <97068125+Gabriel-Sousa-Amorim@users.noreply.github.com>
2024-01-23 12:27:58 -0300

    Create main.js

---
+++ Workspace/Javascript/05_For_While/Exercicio_Moodle/main.js
@@ -0,0 +1,14 @@
+var i = 1;
+        console.log("1) While");
+        while(i <= 50) {
+            console.log(i++);
+        }
+        console.log("2) Do & While");
+        i = 10;
+        do {
+            console.log(i--);
+        } while (i >= 0);
+        console.log("3) For");
+        for (let i = 0; i <= 100; i +=2) {
+            console.log(i);
+        };
